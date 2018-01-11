80f0dbcc526f7d5c28c42c5ffe07c39e9e51bd59
Aniket468 <33116513+Aniket468@users.noreply.github.com>
2018-01-11 23:36:22 +0530

    added navigation drawer

--- app/src/main/java/com/example/aniketkumar/test/Feedback.java
+++ app/src/main/java/com/example/aniketkumar/test/Feedback.java
@@ -2,11 +2,14 @@ package com.example.aniketkumar.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Feedback extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//For FullScreen
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
     }
--- app/src/main/java/com/example/aniketkumar/test/MainActivity.java
+++ app/src/main/java/com/example/aniketkumar/test/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.aniketkumar.test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
@@ -22,15 +29,36 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private FloatingActionButton fab;
     private String[] activityTitles;
-    private  Handler mHandler;
+    private Handler mHandler;
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
+    // tags used to attach the intent
+    private static final String TAG_My_ACCOUNT = "my_account";
+    private static final String TAG_DEALS = "my_deal";
+    private static final String TAG_APP_TUTORIAL = "app_tutorial";
+    private static final String TAG_LOGOUT = "logout";
+    private static final String TAG_FEEDBACK = "feedback";
+    public static String CURRENT_TAG = TAG_My_ACCOUNT;
+    private static final String urlNavHeaderBg = "R.drawable.tutorial";
+    private static final String urlProfileImg = "R.drawable.account";
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        MenuItem mn;
+        mn=navigationView.getMenu().getItem(0);
+        mn.setChecked(true);
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//For FullScreen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mHandler = new Handler();
-
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -52,5 +80,126 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        MenuItem mn;
+        mn=navigationView.getMenu().getItem(0);
+        mn.setChecked(true);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+        // load nav menu header data
+        loadNavHeader();
+
+        // initializing navigation menu
+        setUpNavigationView();
+    }
+
+    private void loadNavHeader() {
+        // name, website
+        txtName.setText("Aniket");
+        txtWebsite.setText("www.aniket.com");
+
+        // loading header background image
+        Glide.with(this).load(urlNavHeaderBg)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgNavHeaderBg);
+        // Loading profile image
+        Glide.with(this).load(urlProfileImg)
+                .crossFade()
+                .thumbnail(0.5f)
+                .bitmapTransform(new CircleTransform(this))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgProfile);
+        // showing dot next to logout label
+        navigationView.getMenu().getItem(4).setActionView(R.layout.menu);
+    }
+
+
+    private void setUpNavigationView() {
+        MenuItem mn;
+        mn=navigationView.getMenu().getItem(0);
+        mn.setChecked(true);
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        navItemIndex = 0;
+                        drawer.closeDrawers();
+                        break;
+                    case R.id.nav_account:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_My_ACCOUNT;
+                        menuItem.setChecked(false);
+                        startActivity(new Intent(MainActivity.this, My_account.class));
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.nav_deals:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_DEALS;
+                        startActivity(new Intent(MainActivity.this, My_deals.class));
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.nav_app_tutorial:
+                        navItemIndex = 3;
+                        CURRENT_TAG = TAG_APP_TUTORIAL;
+
+                        break;
+                    case R.id.nav_logout:
+                        navItemIndex = 4;
+                        CURRENT_TAG = TAG_LOGOUT;
+                        break;
+                    case R.id.nav_feedback:
+                        navItemIndex = 5;
+                        CURRENT_TAG = TAG_FEEDBACK;
+                        startActivity(new Intent(MainActivity.this, Feedback.class));
+                        drawer.closeDrawers();
+                        return true;
+
+                    case R.id.nav_about_us:
+                        // launch new intent instead of loading fragment
+                        startActivity(new Intent(MainActivity.this, About_us.class));
+                        drawer.closeDrawers();
+                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+                //Checking if the item is in checked state or not, if not make it in checked state
+
+//                if (menuItem.isChecked()) {
+//                    menuItem.setChecked(false);
+//                } else {
+//                    menuItem.setChecked(true);
+//                }
+                //menuItem.setChecked(true);
+
+
+                return true;
+            }
+        });
+
     }
 }
+
