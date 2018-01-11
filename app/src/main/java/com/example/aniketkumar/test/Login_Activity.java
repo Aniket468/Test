f7a2b3721eb8eebd8d0ca6c41163eac7f80b45ef
utkarsh22garg <utkarsh22garg@yahoo.com>
2018-01-11 22:01:24 +0530

    Add the login page

---
+++ app/src/main/java/com/example/aniketkumar/test/Login_Activity.java
@@ -0,0 +1,31 @@
+package com.example.aniketkumar.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by MNNIT on 1/11/2018.
+ */
+
+public class Login_Activity extends AppCompatActivity {
+
+    Button login_button;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        login_button= (Button) findViewById(R.id.login_press);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login_Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
